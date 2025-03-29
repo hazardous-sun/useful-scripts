62cc96bb25943687d4ae38638ba9d4df29df2bd9
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-29 19:16:14 -0300

    added adjust-git-emails.sh

--- custom-git/adjust-git-emails.sh
+++ custom-git/adjust-git-emails.sh
@@ -3,6 +3,12 @@
 RED="\033[0;31m"
 NC="\033[0m"
 
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# This script may be dangerous.
+# It messes with SHA1 git objects. Execute it with caution.
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# This script sets all commits e-mails and users to the ones set under your local "git config user.email" and "git config user.name" values. 
+
 main() {
     # Warn the user of what the chaos this script may generate
     echo -ne "${RED}Are you SURE you want to change the commit e-mails from this repository? It will probably create a conflicts hell that you'll have to manually fix, not to mention that the only way to rollback will be recloning the project...${NC}"
