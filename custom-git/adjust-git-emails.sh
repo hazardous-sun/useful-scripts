9e829436783578e3760310c4b211f27365fcba36
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-29 19:10:52 -0300

    added adjust-git-emails.sh

---
+++ custom-git/adjust-git-emails.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+RED="\033[0;31m"
+NC="\033[0m"
+
+main() {
+    # Warn the user of what the chaos this script may generate
+    echo -ne "${RED}Are you SURE you want to change the commit e-mails from this repository? It will probably create a conflicts hell that you'll have to manually fix, not to mention that the only way to rollback will be recloning the project...${NC}"
+    
+    # Read user input
+    read -r user_input
+
+    # Convert the input to lower case
+    choice=$(echo "$user_input" | '[:upper:]' '[:lower:]')
+
+    # Check if the user agreed to run the script 
+    if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
+        echo "Starting the rebase..."
+        git rebase -r --root --exec "git commit --amend --no-edit --reset-author"
+        echo "Finished rebasing"
+    fi
+}
