2b940a04d79b1f48229bc37c1dd34a8d5c8892f9
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-31 20:53:35 -0300

    added color to the messages

--- custom-git/push-repos.sh
+++ custom-git/push-repos.sh
@@ -7,6 +7,7 @@
 PROJECTS="$HOME/Projects/"
 WARNING="\033[33m"
 ERROR="\033[31m"
+INFO='\033[0;36m'
 NC="\033[0m"
 
 printStatus() {
@@ -17,21 +18,21 @@ printStatus() {
     if [ ${#uncommitted[@]} -ne 0 ]; then
         echo -e "${WARNING}The following directories contain uncommitted changes:${NC}"
         for dir in "${uncommitted[@]}"; do
-            echo "$dir"
+            echo -e "${WARNING}$dir${NC}"
         done
     fi
 
     if [ ${#unpushed[@]} -ne 0 ]; then
         echo -e "${WARNING}The following directories have commits not yet pushed:${NC}"
         for dir in "${unpushed[@]}"; do
-            echo "$dir"
+            echo -e "${WARNING}$dir${NC}"
         done
     fi
 
     if [ ${#failed[@]} -ne 0 ]; then
         echo -e "${ERROR}Failed to push changes in the following directories:${NC}"
         for dir in "${failed[@]}"; do
-            echo "$dir"
+            echo -e "${ERROR}$dir${NC}"}
         done
     fi
 }
@@ -96,7 +97,7 @@ setProjectsDir() {
             printUsage
             exit 1
         fi
-        echo "Projects directory set to '$PROJECTS'"
+        echo "${INFO}Projects directory set to '$PROJECTS'${NC}"
     fi
 }
 
@@ -105,7 +106,7 @@ main() {
     setProjectsDir "$@"
 
     # CD into Projects directory
-    cd "$PROJECTS" || { echo "Failed to cd to $PROJECTS"; exit 1; }
+    cd "$PROJECTS" || { echo -e "${ERROR}Failed to cd to $PROJECTS${NC}"; exit 1; }
 
     # Variables for tracking status
     uncommittedDirectories=()
@@ -115,10 +116,10 @@ main() {
     # Iterate over directories inside $PROJECTS
     for dir in */; do
         [ -d "$dir" ] || continue  # Skip if not a directory
-        echo "Checking $dir"
+        echo -e "${INFO}Checking $dir${NC}"
         
         # CD into dir
-        cd "$dir" || { echo "Failed to enter $dir"; continue; }
+        cd "$dir" || { echo -e "${ERROR}Failed to enter $dir${NC}"; continue; }
   
         # Check if $dir is a git directory 
         if git rev-parse --is-inside-work-tree &>/dev/null; then
@@ -133,7 +134,7 @@ main() {
                     ;;
             esac
         else
-            echo "$dir is not a git repository"
+            echo -e "${WARNING}$dir is not a git repository${NC}"
         fi
         
         # Return to Projects directory
